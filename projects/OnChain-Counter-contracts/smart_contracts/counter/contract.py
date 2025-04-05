8c222aa946cf39da6a0360937f3517133068e127
maroti <vaibhavspatre@gmail.com>
2025-04-05 13:10:53 +0530

    project intialised and counter contract created

--- projects/OnChain-Counter-contracts/smart_contracts/counter/contract.py
+++ projects/OnChain-Counter-contracts/smart_contracts/counter/contract.py
@@ -1,8 +1,24 @@
-from algopy import ARC4Contract, String
+from algopy import *
 from algopy.arc4 import abimethod
 
 
 class Counter(ARC4Contract):
+
+    count: UInt64
+
+    def __init__(self) -> None:
+        self.count = UInt64(0)
+
     @abimethod()
-    def hello(self, name: String) -> String:
-        return "Hello, " + name
+    def incr_counter(self)-> UInt64:
+        self.count += UInt64(1)
+        return self.count
+    
+    @abimethod(readonly=True)
+    def get_counter(self) -> UInt64:
+        return self.count
+    
+    @abimethod()
+    def decr_counter(self)-> UInt64:
+        self.count -= UInt64(1)
+        return self.count
